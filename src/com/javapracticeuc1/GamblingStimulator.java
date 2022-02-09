563662bf3c04ff4c3a6c990540f280aaee8b5894
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-02-09 12:30:07 +0530

    Added:Gambler starting stake 00 everday & bet every game

--- src/com/javapracticeuc1/GamblingStimulator.java
+++ src/com/javapracticeuc1/GamblingStimulator.java
@@ -4,6 +4,7 @@ public class GamblingStimulator {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Gambling Simulator");
-	}
-		
+		int stake = 100;
+		int bet = 1;			
+	}	
 }
