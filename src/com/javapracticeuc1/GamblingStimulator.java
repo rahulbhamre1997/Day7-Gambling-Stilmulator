0d3488f155b983cb8f376032535752bb5e25140c
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-02-09 15:20:43 +0530

    Added:gambling win or loss

--- src/com/javapracticeuc1/GamblingStimulator.java
+++ src/com/javapracticeuc1/GamblingStimulator.java
@@ -2,9 +2,25 @@ package com.javapracticeuc1;
 
 public class GamblingStimulator {
 	public static void main(String[] args) {
+
 		
-		System.out.println("Welcome to Gambling Simulator");
-		int stake = 100;
-		int bet = 1;			
+			 int stake = 100 ;	
+			 int bet = 1;
+			 System.out.println("Welcome to Gambling Simulator");
+				
+
+				System.out.println("Gambler start with stake of $" + stake);
+					int TRIAL = (int) Math.floor(Math.random() * 2);
+				System.out.println("Value Is " +TRIAL);
+				if (TRIAL == 0) {   
+					System.out.println("Gambler won game");
+					stake++; 	
+				} else {
+					System.out.println("Gambler loos game");
+					stake--;	
+				}
+				System.out.println("Gambler having stake after bet are of $ " + stake);
+				
 	}	
+
 }
